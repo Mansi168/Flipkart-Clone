394a4d5568f85ee2094fca88480a9c0e52e9b6a2
Mansi <mansins168@gmail.com>
2023-02-08 01:49:30 +0530

    server running successfully

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ import express from 'express';
 
 const app = express();
 
-const PORT = 8000;
+const PORT = 8081;
 
-app.listen(PORT, () => console.log(`Server is running successfully on port 8000`));
+app.listen(PORT, () => console.log(`Server is running successfully on port 8081`));
