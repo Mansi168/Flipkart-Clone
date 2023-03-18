34c0f9e1634d07700ec07eaacb5beb774cd8d21f
Mansi <mansins168@gmail.com>
2023-03-18 23:25:24 +0530

    db setup complete

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import Connection from './database/db.js';
+import DefaultData from './default.js';
 
 
 const app = express();
@@ -13,5 +14,7 @@ const PORT = 8081;
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
-Connection(USERNAME,PASSWORD );   
+Connection(USERNAME,PASSWORD);   
 app.listen(PORT, () => console.log(`Server is running successfully on port ${PORT}`));
+
+DefaultData();
