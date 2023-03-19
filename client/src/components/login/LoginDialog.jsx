3cfbcacea5349d5b5cc98dc87009c4fc97122d79
Mansi <mansins168@gmail.com>
2023-03-19 19:34:38 +0530

    login dialog done

--- client/src/components/login/LoginDialog.jsx
+++ client/src/components/login/LoginDialog.jsx
@@ -1,15 +1,19 @@
 
 import { Box, Dialog, TextField , Typography, Button, styled} from "@mui/material";
 
-const Component = styled.Box`
-    height: 70vh;
-    width: 90vh;
+const Component = styled(Box)`
+    height: 90vh;
+    width: 100vh;
 `;
 const Image = styled(Box)`
-    background: #2874f0 url(https://rukminim1.flixcart.com/flap/3006/433/image/4789bc3aefd54494.jpg?q=50) center 85% no-repeat;
-    height: 100%;
-    width: 40%;
+    background: #2874f0 url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png) center 85% no-repeat;
+    height: 90%;
+    width: 25%;
     padding: 45px 35px; 
+    & > p, & > h5{
+        color: #FFFFFF;
+        font-weight: 600;
+    }
 `;
 const Wrapper = styled(Box)`
     display: flex;
@@ -17,7 +21,7 @@ const Wrapper = styled(Box)`
     padding: 25px 35px;
     flex: 1;
     & > div, & > button, & > p{
-        margin-top: 25px;
+        margin-top: 20px;
     }
 `;
 
@@ -38,12 +42,25 @@ const RequestOTP = styled(Button)`
     box-shadow: 0 2px 4px 0 rgb(0 0 0/ 20%);
 `;
 
+const Text = styled(Typography)`
+    font-size: 12px;
+    color: #878787;
+`;
+
+const CreateAccount = styled(Typography)`
+    font-size: 14px;
+    text-align: center;
+    color: #2874f0;
+    font-weight: 600;
+    cursor: pointer;
+`;
+
 const LoginDialog = ({open, setOpen}) => {
     const handleClose = () =>{
         setOpen(false);
     }
     return(
-        <Dialog open={open} onClose={handleClose}>
+        <Dialog open={open} onClose={handleClose} PaperProps={{ sx:{maxWidth: 'unset'}}}>
             <Component>
                 <Box style={{display:'flex' ,height:'100%'}}>
                     <Image>
@@ -53,11 +70,11 @@ const LoginDialog = ({open, setOpen}) => {
                     <Wrapper>    
                         <TextField variant="standard" label="Enter Email/Mobile number"/>
                         <TextField variant="standard" label="Enter Password"/>
-                        <Typography>By continuing, you agree to Flipkart's terms of use and Privacy Policy</Typography>
+                        <Text>By continuing, you agree to Flipkart's terms of use and Privacy Policy</Text>
                         <LoginButton>Login</LoginButton>
-                        <Typography>OR</Typography>
+                        <Typography style={{textAlign: 'center'}}>OR</Typography>
                         <RequestOTP>Request OTP</RequestOTP>
-                        <Typography>New to Flipkart? Create an account</Typography>
+                        <CreateAccount>New to Flipkart? Create an account</CreateAccount>
                     </Wrapper>   
                 </Box>
             </Component>
